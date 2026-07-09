785197a0656509cae9b1a289548cfd2daf3c0dbc
Meet-301 <pujarameet301@gmail.com>
2026-07-09 09:19:40 +0000

    Jul 9, 2026, 2:49 PM

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ import cors from "cors"
 const app = express() //! new server instance of express application
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
 }))
+
+app.use(express.json({limit: "16kb"})) //! for data that comes from json(it'll accept 16kb)
+app.use(express.urlencoded({limit: "16kb"})) //! for data that comes from URL(it'll accept 16kb)
+app.use(express.static("public")) //! for assets that will be available publicly via folder named "public"
+app.use(cookieParser()) //! to set and get cookies in user browser
 
 export default app
