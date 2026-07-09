cbbcb3b06614d0b2cdbbbe6e0455dd14becfe08b
Meet-301 <pujarameet301@gmail.com>
2026-07-09 02:19:06 +0000

    Jul 9, 2026, 7:49 AM

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
 
 ( () => {
-    
+    try {
+        
+    } catch (error) {
+        console.error("Error: ", error)
+    }
 } )()
