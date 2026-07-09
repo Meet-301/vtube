13b338e7013731ed784947b755a2e8ce976e55e7
Meet-301 <pujarameet301@gmail.com>
2026-07-09 06:30:04 +0000

    Jul 9, 2026, 12:00 PM

---
+++ src/app.js
@@ -0,0 +1,3 @@
+import express from "express"
+
+const app = express() //! new server instance of express application
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import connectDB from "./db/db.js";
-import dotenv from "dotenv"
-import express from "express";
+import dotenv from "dotenv";
 import dns from "dns";
 
 //! Set DNS servers before any database connection
 //! (force node.js to use trusted DNS resolution providers)
 //! (e.g. cloudflare or google)
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
-
-const app = express() //! new server instance of express application
 
 dotenv.config({
     path: './env'
