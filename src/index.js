b47804ef253e7e7a84dde20d3f85fc7248dd73b7
Meet-301 <pujarameet301@gmail.com>
2026-07-09 03:55:24 +0000

    Jul 9, 2026, 9:25 AM

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import connectDB from "./db/db.js";
 import dotenv from "dotenv"
 import express from "express";
 import dns from "dns";
-// Set DNS servers before any database connection
-dns.setServers(["1.1.1.1", "8.8.8.8"]);   
+
+//! Set DNS servers before any database connection
+//! (force node.js to use trusted DNS resolution providers)
+//! (e.g. cloudflare or google)
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const app = express()
 
