edb27fdab48ca3e59d2beab50185915da29e5229
Meet-301 <pujarameet301@gmail.com>
2026-07-09 05:44:10 +0000

    Jul 9, 2026, 11:14 AM

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import dns from "dns";
 //! Set DNS servers before any database connection
 //! (force node.js to use trusted DNS resolution providers)
 //! (e.g. cloudflare or google)
-dns.setServers(["1.1.1.1", "8.8.8.8"]);
+// dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const app = express()
 
