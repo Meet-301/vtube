fdd72f5229726117180bb6f83e3e8373633ab130
Meet-301 <pujarameet301@gmail.com>
2026-07-09 02:26:13 +0000

    Jul 9, 2026, 7:56 AM

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
 import express from "express";
 
-const 
+const app = express()
 
 ( () => {
     try {
         mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("Error: ", error);
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`app is listening on port ${process.env.PORT}`);
+        })
     } catch (error) {
         console.error("Error: ", error)
     }
