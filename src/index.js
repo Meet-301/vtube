355c1bce571cfc16c75954cb4b2722d49f5be70b
Meet-301 <pujarameet301@gmail.com>
2026-07-09 06:38:26 +0000

    Jul 9, 2026, 12:08 PM

--- src/index.js
+++ src/index.js
@@ -14,9 +14,11 @@ dotenv.config({
 
 connectDB()
 .then(() => {
+    app.on("e", (error) => {
+
+    })
     app.listen(process.env.PORT || 5000, () => {
         console.log(`Server is running at port ${process.env.PORT}`);
-        
     })
 })
 .catch((error) => console.log(`DB Connection Error: ${error}`))
