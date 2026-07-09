e5ca7da38a759f27b2b674f90fe96ac16a0acb19
Meet-301 <pujarameet301@gmail.com>
2026-07-09 02:49:54 +0000

    Jul 9, 2026, 8:19 AM

--- src/db/db.js
+++ src/db/db.js
@@ -4,7 +4,12 @@ import { DB_NAME } from "../constants";
 const connectDB = async() => {
     try {
         const conn = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-    } catch (error) {
+        console.log(`connected database! DB host: ${conn.connection.host}`);
         
+    } catch (error) {
+        console.log(`Connection error: ${error}`);
+        process.exit(1)
     }
 }
+
+export default connectDB
