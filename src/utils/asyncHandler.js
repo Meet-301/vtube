34896031558088dcac03054000788a05c6e7bbde
Meet-301 <pujarameet301@gmail.com>
2026-07-14 11:09:26 +0530

    kept previous code

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,23 +1,23 @@
 //! wrapper class to wrap the functions
 
 //todo first approach
-const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {
-        return Promise.resolve(requestHandler(req, res, next))
-        .catch((err) => next(err))
-    }
-}
-
-//todo second approach
-// const asyncHandler = (fn) => async (req, res, next) => {
-//     try {
-//         await fn(req, res, next)
-//     } catch (error) {
-//         res.status(error.code || 500).json({
-//             success: false,
-//             message: error.message
-//         })
+// const asyncHandler = (requestHandler) => {
+//     return (req, res, next) => {
+//         return Promise.resolve(requestHandler(req, res, next))
+//         .catch((err) => next(err))
 //     }
 // }
+
+//todo second approach
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
 
 export default asyncHandler
