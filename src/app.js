68b8393b10556ea76e2dfb037bfbbea22e1f0b66
Meet-301 <pujarameet301@gmail.com>
2026-07-14 12:12:56 +0530

    removed limit of data reqeust in url encoding format

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors({
 }))
 
 app.use(express.json({limit: "16kb"})) //! for data that comes from json(it'll accept 16kb)
-app.use(express.urlencoded({ limit: "16kb", extended: true })) 
+app.use(express.urlencoded({ extended: true })) 
 //! for data that comes from HTML form
 app.use(express.static("public")) //! for assets that will be available publicly via folder named "public"
 app.use(cookieParser()) //! to set and get cookies in user browser
