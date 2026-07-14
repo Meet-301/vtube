9a5ec5bae240e9c3385cc23fd4949dfaa5b7f59c
Meet-301 <pujarameet301@gmail.com>
2026-07-14 11:55:56 +0530

    completed user registration comtroller code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,26 +1,87 @@
 import asyncHandler from "../utils/asyncHandler.js";
-import ApiError from "../utils/ApiError.js"
+import ApiError from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import uploadOnCloudinary from "../utils/cloudinary.js";
+import ApiResponse from "../utils/ApiResponse.js";
 
-const registerUser = asyncHandler(async(req, res) => {
-    //! get user details from frontend
-    //! validation
-    //! check if user already exists: username, email
-    //! check for images and avatar
-    //! upload them to cloudinary, avatar
-    //! create user object - create entry in DB
-    //! remove password and refresh token fields from response
-    //! check for user creation
-    //! return response
+const registerUser = asyncHandler(async (req, res) => {
+   //! get user details from frontend
+   const { fullName, email, username, password } = req.body;
+   console.log(req.body);
 
-    const {fullName, email, username, password} = req.body
-    console.log(req.body)
+   //! validation
+   const isValidEmail = (email) => {
+      const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      return emailRegex.test(email);
+   };
 
-    if (
-        [fullName, email, username, password].some((field) => field?.trim() === "")
-    ) {
-        throw new ApiError(400, "All fields are required")
-    }
+   const isValidPassword = (password) => {
+      const passwordRegex =
+         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+      return passwordRegex.test(password);
+   };
 
-})
+   if (
+      [fullName, email, username, password].some(
+         (field) => field?.trim() === ""
+      )
+   ) {
+      throw new ApiError(400, "All fields are required");
+   } else if (!isValidEmail(email)) {
+      throw new ApiError(400, "Invalid email format");
+   } else if (!isValidPassword(password)) {
+      throw new ApiError(400, "Invalid password format");
+   }
 
-export default registerUser
+   //! check if user already exists(by username and email)
+   const existedUser = User.findOne({
+      $or: [{ username }, { email }],
+   });
+
+   if (existedUser) {
+      throw new ApiError(409, "User already exists");
+   }
+
+   //! check for images and avatar
+   const avatarLocalPath = req.files?.avatar[0]?.path;
+   const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+   if (!avatarLocalPath) {
+      throw new ApiError(400, "Avatar file is required");
+   }
+
+   //! upload them to cloudinary, avatar
+   const avatar = await uploadOnCloudinary(avatarLocalPath);
+   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+   if (!avatar) {
+      throw new ApiError(400, "Avatar file is required");
+   }
+
+   //! create user object - create entry in DB
+   const user = User.create({
+      email,
+      password,
+      username: username.toLowerCase(),
+      fullName,
+      avatar: avatar.url,
+      coverImage: coverImage?.url || "",
+   });
+
+   //! remove password and refresh token fields from response
+   const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+   )
+
+   //! check for user creation
+   if(!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering user")
+   }
+
+   //! return response
+   return res.status(201).json(
+    new ApiResponse(200, createdUser, "User Registered Successfully")
+   )
+});
+
+export default registerUser;
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,21 +2,21 @@ import { Router } from "express";
 import registerUser from "../controllers/user.controller.js";
 import { multerUpload } from "../middlewares/multer.middleware.js";
 
-const userRouter = Router()
+const userRouter = Router();
 
 userRouter.route("/register").post(
-    //! set the fields of multer to upload the files before registerUser gets invoked
-    multerUpload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        }, 
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    registerUser
-)
+   //! set the fields of multer to upload the files before registerUser gets invoked
+   multerUpload.fields([
+      {
+         name: "avatar",
+         maxCount: 1,
+      },
+      {
+         name: "coverImage",
+         maxCount: 1,
+      },
+   ]),
+   registerUser
+);
 
-export default userRouter
+export default userRouter;
