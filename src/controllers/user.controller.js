2b3192a80d81cfc8fce2acb50e009364728dc041
Meet-301 <pujarameet301@gmail.com>
2026-07-15 19:56:26 +0530

    implemented login user functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,25 @@ import { User } from "../models/user.model.js";
 import uploadOnCloudinary from "../utils/cloudinary.js";
 import ApiResponse from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshTokens = async (userId) => {
+   try {
+      const user = await User.findById(userId); //! find the user by given id
+      const accessToken = user.generateAccessToken(); //! get access token
+      const refreshToken = user.generateRefreshToken(); //! get refresh token
+
+      user.refreshToken = refreshToken;
+
+      await user.save({ validateBeforeSave: false });
+
+      return { accessToken, refreshToken };
+   } catch (error) {
+      throw new ApiError(
+         500,
+         "Something went wrong while generating access and refresh token"
+      );
+   }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
    //! get user details from frontend
    const { fullName, email, username, password } = req.body;
@@ -43,8 +62,8 @@ const registerUser = asyncHandler(async (req, res) => {
    }
 
    //! check for images and avatar
-   const avatarLocalPath = req.files?.avatar[0]?.path;
-   const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+   const avatarLocalPath = req.files?.avatar?.[0]?.path;
+   const coverImageLocalPath = req.files.coverImage?.[0].path;
 
    if (!avatarLocalPath) {
       throw new ApiError(400, "Avatar file is required");
@@ -86,4 +105,59 @@ const registerUser = asyncHandler(async (req, res) => {
       .json(new ApiResponse(200, createdUser, "User Registered Successfully"));
 });
 
-export default registerUser;
+const loginUser = asyncHandler(async (req, res) => {
+   //! request body -> data
+   const { email, password } = req.body;
+
+   //! check whether email is provided or not
+   if (!email) {
+      throw new ApiError(400, "Email is required");
+   }
+
+   //! find the user
+   const user = await User.findOne({ email });
+
+   //! if user not found
+   if (!user) {
+      throw new ApiError(404, "User not found");
+   }
+
+   //! check whether password is correct or not
+   const isPasswordValid = await user.isPasswordCorrect(password);
+
+   if (!isPasswordValid) {
+      throw new ApiError(401, "Password is incorrect");
+   }
+
+   //! generate access and refresh tokens
+   const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+      user._id
+   );
+
+   const loggedInUser = User.findById(user._id).select(
+      "-password -refreshToken"
+   );
+
+   const options = {
+      httpOnly: true,
+      secure: true,
+   };
+
+   return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", refreshToken, options)
+      .json(
+         new ApiResponse(
+            200,
+            {
+               user: loggedInUser,
+               accessToken,
+               refreshToken,
+            },
+            "User logged in successfully"
+         )
+      );
+});
+
+export { registerUser, loginUser };
