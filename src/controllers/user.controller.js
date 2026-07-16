c27416ddb863e7591a03f7dfe0ffab3554ac47fe
Meet-301 <pujarameet301@gmail.com>
2026-07-16 16:22:37 +0530

    fixed some bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -134,7 +134,7 @@ const loginUser = asyncHandler(async (req, res) => {
       user._id
    );
 
-   const loggedInUser = User.findById(user._id).select(
+   const loggedInUser = await User.findById(user._id).select(
       "-password -refreshToken"
    );
 
