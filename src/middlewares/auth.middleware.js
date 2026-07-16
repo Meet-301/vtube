49c112820f7f184cb314948d0aa1003b01d014d2
Meet-301 <pujarameet301@gmail.com>
2026-07-16 08:13:13 +0530

    wrapped the code with trycatch block

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,29 +4,34 @@ import jwt from "jsonwebtoken";
 import { User } from "../models/user.model";
 
 const verifyJWT = asyncHandler(async (req, res, next) => {
-   //! get the token from either cookies or request headers(authorization header)
-   const token =
-      req.cookies?.accessToken ||
-      req.header("Authorization")?.replace("Bearer ", "");
+   //! this trycatch block is required because errors occurred by verify() method are not covered by ApiError
+   try {
+      //! get the token from either cookies or request headers(authorization header)
+      const token =
+         req.cookies?.accessToken ||
+         req.header("Authorization")?.replace("Bearer ", "");
 
-   if (!token) {
-      throw new ApiError(401, "Unauthorized request");
+      if (!token) {
+         throw new ApiError(401, "Unauthorized request");
+      }
+
+      //! verify whether the token is correct or not
+      const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+      const user = await User.findById(decodedToken._id).select(
+         "-password -refreshToken"
+      );
+
+      if (!user) {
+         throw new ApiError(401, "Invalid access token");
+      }
+
+      req.user = user;
+      //! because downstream flow(e.g. middlewares, controllers) can use this authenticated user directly
+      next();
+   } catch (error) {
+      throw new ApiError(401, error?.message || "Invalid access token");
    }
-
-   //! verify whether the token is correct or not
-   const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-   const user = await User.findById(decodedToken._id).select(
-      "-password -refreshToken"
-   );
-
-   if (!user) {
-      throw new ApiError(401, "Invalid access token");
-   }
-
-   req.user = user; 
-   //! because downstream flow(e.g. middlewares, controllers) can use this authenticated user directly
-   next();
 });
 
 export { verifyJWT };
