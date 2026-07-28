cc31a55fe185643f6e76244762522b32349b3825
Meet-301 <pujarameet301@gmail.com>
2026-07-28 15:09:48 +0530

    added "fetch routes"

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -65,4 +65,67 @@ const toggleLikes = asyncHandler(async (req, res) => {
       );
 });
 
-export { toggleLikes }
+const getLikedVideos = asyncHandler(async (req, res) => {
+   const likedVideos = await Like.aggregate([
+      {
+         $match: {
+            likedBy: new mongoose.Types.ObjectId(req.user._id),
+         },
+      },
+      {
+         $lookup: {
+            from: "videos",
+            localField: "video",
+            foreignField: "_id",
+            as: "video",
+            pipeline: [
+               {
+                  $project: {
+                     description: 0,
+                     videoFile: 0,
+                     isPublished: 0,
+                     likes: 0,
+                  },
+               },
+               {
+                  $lookup: {
+                     from: "users",
+                     localField: "owner",
+                     foreignField: "_id",
+                     as: "owner",
+                     pipeline: [{ $project: { fullName: 1, avatar: 1 } }],
+                  },
+               },
+               {
+                  $addFields: {
+                     owner: { $first: "$owner" },
+                  },
+               },
+            ],
+         },
+      },
+      {
+         $addFields: {
+            video: { $first: "$video" },
+         },
+      },
+      {
+         $project: {
+            video: 1,
+            createdAt: 1,
+         },
+      },
+   ]);
+
+   return res
+      .status(200)
+      .json(
+         new ApiResponse(
+            200,
+            likedVideos[0],
+            "Liked videos fetched successfully"
+         )
+      );
+});
+
+export { toggleLikes, getLikedVideos };
--- src/routes/like.route.js
+++ src/routes/like.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { toggleLikes } from "../controllers/like.controller.js";
+import { getLikedVideos, toggleLikes } from "../controllers/like.controller.js";
 
 const likeRouter = Router();
 
 likeRouter.route("/toggle/:videoId").patch(verifyJWT, toggleLikes);
+likeRouter.route("/all").get(verifyJWT, getLikedVideos)
 
 export default likeRouter;
