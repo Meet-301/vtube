8a55af0541b925bb46e9c007c5bdb7ad4fa60e27
Meet-301 <pujarameet301@gmail.com>
2026-07-29 18:14:12 +0530

    declared comment route

--- src/app.js
+++ src/app.js
@@ -22,10 +22,12 @@ app.use(cookieParser()); //! to set and get cookies in user browser
 import userRouter from "./routes/user.route.js";
 import videoRouter from "./routes/video.route.js";
 import likeRouter from "./routes/like.route.js";
+import commentRouter from "./routes/comment.route.js";
 
 //! routes declaration
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/videos", videoRouter)
-app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/comments", commentRouter);
 
 export default app;
