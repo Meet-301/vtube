aa4c584faef3c506812dc199d6ab1503ce9dfc2d
Meet-301 <pujarameet301@gmail.com>
2026-07-31 19:34:52 +0530

    declared subscription route

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ import videoRouter from "./routes/video.route.js";
 import likeRouter from "./routes/like.route.js";
 import commentRouter from "./routes/comment.route.js";
 import playlistRouter from "./routes/playlist.route.js";
+import subscriptionRouter from "./routes/subscription.route.js";
 
 //! routes declaration
 app.use("/api/v1/users", userRouter);
@@ -31,5 +32,6 @@ app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/playlists", playlistRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 export default app;
