a6f68ee41f56fbc3190f2dd6cd7129e68928c29f
Meet-301 <pujarameet301@gmail.com>
2026-08-05 17:01:12 +0530

    added fields for password reset functionality

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,6 +53,12 @@ const userSchema = new Schema(
       emailVerificationExpiry: {
          type: Date,
       },
+      passwordResetToken: {
+         type: String,
+      },
+      passwordResetExpiry: {
+         type: Date,
+      },
    },
    { timestamps: true }
 );
