340b3cca6dd18dbcc036365e9d0d6d10d7436a5e
Meet-301 <pujarameet301@gmail.com>
2026-08-05 20:54:30 +0530

    mailer functionality done

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -12,7 +12,7 @@ const transporter = nodemailer.createTransport({
 
 const sendEmail = async ({ to, subject, html }) => {
    const info = await transporter.sendMail({
-      from: `Vtube Team ${process.env.SMTP_MAIL}`,
+      from: `Vtube ${process.env.SMTP_MAIL}`,
       to,
       subject,
       html,
