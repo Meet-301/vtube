83d6605eb54699fd99c861289ab8febfc3a28dc3
Meet-301 <pujarameet301@gmail.com>
2026-08-09 13:12:41 +0530

    added schema for timestamp notes functionality

---
+++ src/models/timestampNote.model.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+const timestampNoteSchema = new Schema(
+   {
+      userId: {
+         type: Schema.Types.ObjectId,
+         ref: "User",
+      },
+      videoId: {
+         type: Schema.Types.ObjectId,
+         ref: "Video",
+      },
+      timestamp: {
+         type: Number,
+         required: true,
+      },
+      title: {
+         type: String,
+         required: true,
+      },
+      description: {
+         type: String,
+         required: true,
+      },
+   },
+   { timestamps: true }
+);
+
+export const TimestampNote = mongoose.model(
+   "TimestampNote",
+   timestampNoteSchema
+);
