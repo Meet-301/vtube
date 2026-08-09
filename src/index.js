2f7fb5446e14a99ebe7733332afe05364ec6221b
Meet-301 <pujarameet301@gmail.com>
2026-08-09 12:41:38 +0530

    socket.io initial setup done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import connectDB from "./db/db.js";
 import dotenv from "dotenv";
 import dns from "dns";
 import app from "./app.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import ApiResponse from "./utils/ApiResponse.js";
 
 //! Set DNS servers before any database connection
 //! (force node.js to use trusted DNS resolution providers)
@@ -13,14 +16,32 @@ dotenv.config({
    path: "./.env",
 });
 
+const server = createServer(app)
+
+const io = new Server(server)
+
+io.on("connection", (socket) => {
+   console.log(`User connected: ${socket.id}`);
+   
+   socket.on("disconnect", () => {
+      console.log(`User disconnected: ${socket.id}`);
+   })
+})
+
 connectDB().then(() => {
    app.on("error", (error) => {
       console.log(`Application failed: ${error}`);
    });
 
+   app.get("/", (req, res) => {
+      res.json(new ApiResponse(200, {
+         message: "hello"
+      }, "Hello from server"))
+   })
+
    const port = process.env.PORT || 5000;
 
-   app.listen(port, () => {
+   server.listen(port, () => {
       console.log(`Server is running at port ${port}`);
    });
 });
