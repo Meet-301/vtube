05cb1b91a3a8e3d8a9ebd694364437a95e1a820a
Meet-301 <pujarameet301@gmail.com>
2026-09-18 17:18:49 +0530

    fixed minor bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -760,7 +760,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
          $lookup: {
             from: "playlists",
             localField: "_id",
-            foreignField: "owner.id",
+            foreignField: "owner._id",
             as: "playlists",
          }
       },
